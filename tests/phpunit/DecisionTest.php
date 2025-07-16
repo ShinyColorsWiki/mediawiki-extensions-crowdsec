2096d1567d661b0b88c7b17ad8daad555eb2f4d4
MPThLee <mpthlee@gmail.com>
2025-07-16 19:37:10 +0900

    fix: php uses

--- tests/phpunit/DecisionTest.php
+++ tests/phpunit/DecisionTest.php
@@ -21,6 +21,7 @@
 namespace MediaWiki\Extension\CrowdSec\Tests;
 
 use MediaWiki\Extension\CrowdSec\LAPIClient;
+use MediaWiki\Extension\CrowdSec\Tests\CrowdSecMockHttpTrait;
 
 /**
  * @coversDefaultClass \MediaWiki\Extension\CrowdSec
